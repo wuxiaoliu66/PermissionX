355b5d08424715a743e3c691d6bde75e8135e287
wuruoke <wuruoke@kuaishou.com>
2021-08-03 16:59:16 +0800

    完成PermissionX库的开发与测试工作

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.permissionx.wuruokedev.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    makeCallBtn.setOnClickListener {
+      PermissionX.request(this,
+        Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+        if (allGranted) {
+          call()
+        } else {
+          Toast.makeText(this, "You denied $deniedList",Toast.LENGTH_SHORT).show()
+        }
+      }
+    }
+  }
+
+  private fun call() {
+    try {
+      val intent = Intent(Intent.ACTION_CALL)
+      intent.data = Uri.parse("tel:10086")
+      startActivity(intent)
+    } catch (e: SecurityException) {
+      e.printStackTrace()
+    }
   }
 }
